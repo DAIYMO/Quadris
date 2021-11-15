27c0863dadfefb7734a8075b2d232054d4086b57
LANDON TOMKINS <landontomkins@gmail.com>
2021-11-15 15:32:39 -0600

    uncommented sound

--- Code/Quadris/FrmMain.cs
+++ Code/Quadris/FrmMain.cs
@@ -65,8 +65,8 @@ namespace Quadris
             CreateGrid();
             CreateHoldGrid();
             CreateNextGrid();
-            //sndPlayer = new SoundPlayer(Resources.bg_music);
-            //sndPlayer.PlayLooping();
+            sndPlayer = new SoundPlayer(Resources.bg_music);
+            sndPlayer.PlayLooping();
         }
 
         private void CreateGrid()
